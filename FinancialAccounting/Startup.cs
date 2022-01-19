278950b6b1b1d866b214f2130d3f97ff307da525
Mostafa El-Diasty <mostafaeldiasty95@gmail.com>
2022-01-20 00:04:27 +0200

    Add project files.

---
+++ FinancialAccounting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinancialAccounting.Startup))]
+namespace FinancialAccounting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
