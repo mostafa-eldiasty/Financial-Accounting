f0539dc13f83426a4af1d17aa6d3618d0b9c51c9
Mostafa El-Diasty <mostafaeldiasty95@gmail.com>
2022-02-26 22:24:15 +0200

    building enum FormType and adding it to branches View and Controller

--- FinancialAccounting/Areas/Settings/Controllers/BranchesController.cs
+++ FinancialAccounting/Areas/Settings/Controllers/BranchesController.cs
@@ -1,7 +1,10 @@
 ﻿using DataAccess.DTOs;
+using FinancialAccounting.Enums;
 using DataAccess.Models;
 using DataAccess.Repositories;
 using FinancialAccounting.Controllers;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -23,5 +26,50 @@ namespace FinancialAccounting.Areas.Settings.Controllers
             List<BranchDto> branchDto = repository.Get().ToList();
             return View(branchDto);
         }
+
+        // GET: Settings/Branches/Form ( /1 ?FormType= 1 )
+        public ActionResult Form(int id = 0,int FormType = 0)
+        {
+            ViewBag.FormType = FormType;
+            BranchDto branchDto = new BranchDto();
+
+            if (id == 0)
+            {
+                var branch = repository.Get().OrderByDescending(b => b.Code).FirstOrDefault();
+                branchDto.Code = branch != null ? branch.Code + 1 : 1;
+                return View(branchDto);
+            }
+
+            branchDto = repository.GetSingleByExp(x => x.Id == id);
+            return View(branchDto);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Form(BranchDto branchDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(branchDto);
+            }
+
+            if (branchDto.Id == 0)
+            {
+                ViewBag.FormType = FormType.New;
+                branchDto.AddedDate = DateTime.Now;
+                branchDto.AddedUserId = User.Identity.GetUserId();
+            }
+            else
+            {
+                ViewBag.FormType = FormType.Edit;
+                branchDto.UpdatedDate = DateTime.Now;
+                branchDto.UpdatedUserId = User.Identity.GetUserId();
+            }
+
+            repository.AddOrUpdate(branchDto);
+            return View(branchDto);
+        }
     }
 }
+
+
---
+++ FinancialAccounting/Enums/Enums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinancialAccounting.Enums
+{
+    public enum FormType
+    {
+        Details = 0,
+        New = 1,
+        Edit = 2,
+        Delete = 3
+    }
+}
